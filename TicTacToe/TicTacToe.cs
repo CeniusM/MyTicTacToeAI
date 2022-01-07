f5557bf362165b4c9ebf295a4b3e838937bf9c09
CeniusM <cenius.meier@gmail.com>
2022-01-07 12:13:10 +0100

    changes

--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -1,135 +1,69 @@
-namespace CS_TicTacToe
+namespace MyTicTacToe
 {
-    class TicTacToe
+    class TicTacToe // mostly used for AI's
     {
-        public int[,] Board { get; private set; }
-        public int playerTurn { get; private set; }
-        public bool gameRunning { get; private set; }
-        public int winner { get; private set; }
+        public int[] board { get; private set; } = new int[9];
+        public int playerTurn { get; private set; } = 1;
+        public int winner { get; private set; } = 0;
+        public bool running { get; private set; } = true;
         private int moveAmout = 0;
-        public TicTacToe()
+        public bool MakeMove(int move) // the return value is just if i wanna use it to punish ais for making an ilegal move
         {
-            Board = new int[3, 3];
-            playerTurn = 1;
-            gameRunning = true;
-            winner = 0;
-        }
-        public void MakeMove(int[] input)
-        {
-            MakeMove(input[0], input[1]);
-        }
-        public void MakeMove(int i, int j)
-        {
-            if (!gameRunning) return;
-
-            if (Board[i, j] == 0)
+            if (board[move] == 0 && running)
             {
-                Board[i, j] = playerTurn;
+                board[move] = playerTurn;
                 moveAmout++;
 
-                GameoverCheck(i, j);
-
-                if (!gameRunning) return;
-
-                ChangePlayer();
+                if (IsGameOver())
+                {
+                    winner = playerTurn;
+                    running = false;
+                    return true;
+                }
+                else
+                {
+                    SwitchPlayer();
+                }
+                if (moveAmout == 9)
+                {
+                    playerTurn = 0;
+                    return true;
+                }
             }
-
-            //checks if after the ninth move is true and gameovercheck is false, the n stop the game with no winner
-            if (moveAmout == 9)
-                gameRunning = false;
+            return false;
         }
 
-        private void GameoverCheck(int i, int j) // shit code, needs improvment
+        private bool IsGameOver()
         {
-            // Code
-            bool vanret = false;
-            bool lodret = false;
-            bool diagonal = false;
-            int vanretNum = 0;
-            int lodretNum = 0;
-            int diagonal1 = 0;
-            int diagonal2 = 0;
-
-            for (int k = 0; k < 3; k++)
+            for (int i = 0; i < 3; i++)
             {
-                // vandret
-                if (Board[i, k] == playerTurn)
-                    vanretNum++;
-
                 // lodret
-                if (Board[k, j] == playerTurn)
-                    lodretNum++;
+                if (board[0 + (i * 3)] != 0)
+                    if (board[0 + (i * 3)] == board[1 + (i * 3)] && board[1 + (i * 3)] == board[2 + (i * 3)])
+                        return true;
 
-                if (Board[k, k] == playerTurn)
-                    diagonal1++;
+                // vandret
+                if (board[0 + i] != 0)
+                    if (board[0 + i] == board[3 + i] && board[3 + i] == board[6 + i])
+                        return true;
             }
-            if (Board[0, 2] == playerTurn)
-                diagonal2++;
-            if (Board[1, 1] == playerTurn)
-                diagonal2++;
-            if (Board[2, 0] == playerTurn)
-                diagonal2++;
 
-            if (vanretNum == 3)
-                vanret = true;
-            if (lodretNum == 3)
-                lodret = true;
-            if (diagonal1 == 3)
-                diagonal = true;
-            if (diagonal2 == 3)
-                diagonal = true;
+            // diagonalerne
+            if (board[board[0]] != 0)
+                if (board[0] == board[4] && board[0] == board[8])
+                    return true;
+            if (board[2] != 0)
+                if (board[2] == board[4] && board[2] == board[6])
+                    return true;
 
-            if (vanret || lodret || diagonal)
-            {
-                gameRunning = false;
-                winner = playerTurn;
-            }
+            return false;
         }
 
-        private void ChangePlayer()
+
+        private void SwitchPlayer()
         {
             if (playerTurn == 1) playerTurn = 2;
             else playerTurn = 1;
         }
-
-        public static int Get1DField(int x, int y)
-        {
-            int field = x;
-            if (y != 0)
-                field += y * 3;
-
-            return field;
-        }
-
-        public static int[] Get2DField(int num) // returns an reverse y and normla x value
-        {
-            int xCoord = 0;
-            int yCoord = 0;
-
-            while (num > 2)
-            {
-                num -= 3;
-                yCoord++;
-            }
-            xCoord = num;
-
-            int[] output = { yCoord, xCoord };
-
-            return output;
-        }
     }
 }
-/*
-function getCoords(num) {
-  let xCoord = 0;
-  let yCoord = 0;
-  while (num > 2) {
-    console.log(num);
-    num -= 3;
-    yCoord++;
-  }
-  xCoord = num;
-  console.log(board);
-  return [yCoord, xCoord];
-}
-*/
